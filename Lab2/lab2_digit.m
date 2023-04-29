28af2b19c8cf4a6963b715b57cc8c60fe2ba754b
es-OmarHani <es-OmarHani2024@alexu.edu.eg>
2023-04-29 09:45:18 +0300

    fix: Add figures and comments in part_two

--- Lab2/lab2_digit.m
+++ Lab2/lab2_digit.m
@@ -1,6 +1,6 @@
 %% construction from oversampling
 t=0:0.001:1;% time signal
-fs=1000; % 1/0.001
+fs = 1000; % 1/0.001
 y=2*cos(2*pi*5*t);
 
 figure
@@ -17,6 +17,11 @@ end
 zero_added_signal(1:9)=[];
 % Adding zeros enhances the signal display and don't change the
 %spectrum,it changes sampling freq. only
+%  the length of the signal is increased by a factor of 10,
+%  which effectively increases the sampling rate by a factor of 10 as well.
+%  This results in a smoother waveform with higher resolution in the time domain
+%  , and a higher frequency resolution in the frequency domain.
+
 t=linspace(0,1,length(zero_added_signal));
 filtered_signal = filter(B,A,zero_added_signal);
 
@@ -27,7 +32,7 @@ title("oversampled time domian signal")
 
 s=fft(filtered_signal);
 s=fftshift(s);
-fs=10000; %1/0.001 *10 as we added 10 zeros
+fs = 10000; %1/0.001 *10 as we added 10 zeros
 freq=linspace(-fs/2,fs/2,length(s));
 
 subplot(3,1,3)
@@ -38,13 +43,15 @@ title("freq domain oversampled signal")
 %% construction from minimum sampling
 
 t=0:0.1:1; % replace ?? with the suitable number
-fs=10; % fs=1/0.1
-y=2*cos(2*pi*5*t);
+fs = 10; % fs=1/0.1
+y = 2*cos(2*pi*5*t);
 [B,A] = butter(10,5/50,'low' );
+
 zero_added_signal=zeros(1,length(y)*10);
 for i=1:length(y)
 zero_added_signal(i*10)=y(i);
 end
+
 zero_added_signal(1:9)=[];
 t=linspace(0,1,length(zero_added_signal));
 filtered_signal = filter(B,A,zero_added_signal);
@@ -57,7 +64,7 @@ title('minimum sampled signals')
 
 s=fft(filtered_signal);
 s=fftshift(s);
-fs=100; % why 100?? 
+fs = 100; % why 100?? 
 % fs=1/0.1 * 10 as we added 10 zeros later which inreases the sampling freq by 10
 freq=linspace(-fs/2,fs/2,length(s));
 
@@ -118,7 +125,6 @@ title('Original Sine Wave');
 n_vec = [3 4 5 10];
 MSE = zeros(size(n_vec));
 
-figure;
 for i = 1:length(n_vec)
     n = n_vec(i);               % Number of bits for integer or fraction parts
     m = 2*n + 1;                % Number of bits for the whole word, including the sign bit
@@ -128,19 +134,29 @@ for i = 1:length(n_vec)
     e = x - xq;                 % Calculate quantization error
     MSE(i) = mean(e.^2);        % Calculate mean square quantization error
     
-    % Create a figure with 4 columns and 2 rows of subplots
-    subplot(4,1,i);
+    % representation
+    figure;
     plot(t,xq);
     % Add a main title to the figure
-    title('Quantized signal using fi function for n = ', num2str(n) );
+    title('Quantized signal for n = ', num2str(n) );
 end
 
-figure
-stem(n_vec,MSE)
-title("quantization using pi function")
+figure;
+plot(n_vec , MSE , 'r')
 xlabel("number of bits n")
 ylabel("MSE")
+title("quantization using pi function")
 
+
+% Note : Increasing the number of bits for quantization 
+% improves the accuracy of the quantized signal,
+% which results in a lower mean square quantization error.
+% When n is small, 
+% the number of bits used to represent the signal is limited, 
+% which can result in significant quantization errors. 
+% As n increases, 
+% more bits are used to represent the signal, allowing for a more accurate representation of the original signal,
+% and reducing the quantization error.
 
 %% quantization using quantize function
 % Parameters
@@ -163,7 +179,6 @@ title('Original Sine Wave');
 n_vec = [3 4 5 10];
 MSE = zeros(size(n_vec));
 
-figure;
 for i = 1:length(n_vec)
     n = n_vec(i);
     m = 2*n + 1;                % word size
@@ -173,16 +188,16 @@ for i = 1:length(n_vec)
     e = x - double(xq);         % Calculate quantization error
     MSE(i) = mean(e.^2);        % Calculate mean square quantization error
     
-    % Create a figure with 4 columns and 2 rows of subplots
-    subplot(4,1,i);
+    % representation
+    figure;
     plot(t,xq);
     % Add a main title to the figure
-    title('Quantized signal using fi function for n = ', num2str(n) );
+    title('Quantized signal for n = ', num2str(n) );
 end
-figure
-stem(n_vec, MSE)  % Plot mean square quantization error
-title("quantization using qunatizate function")
 
+figure;
+plot(n_vec, MSE , 'r')  % Plot mean square quantization error
+title("quantization using qunatizate function")
 xlabel('Number of bits')
 ylabel('Mean square quantization error')
 
@@ -213,7 +228,6 @@ x_compand = compand(x, mu, max(x), 'mu/compressor');  % Compress signal using co
 n_vec = [3 4 5 10];
 MSE = zeros(size(n_vec));
 
-figure;
 for i = 1:length(n_vec)
     n = n_vec(i);
     m = 2*n + 1;
@@ -224,15 +238,15 @@ for i = 1:length(n_vec)
     e = x - double(x_expand);             % Calculate quantization error
     MSE(i) = mean(e.^2);            % Calculate mean square quantization error
     
-    % Create a figure with 4 columns and 2 rows of subplots
-    subplot(4,1,i);
+    % representation
+    figure;
     plot(t,xq);
     % Add a main title to the figure
-    title('Quantized signal using fi function for n = ', num2str(n) );
+    title('Quantized signal for n = ', num2str(n) );
 end
 
 figure
-stem(n_vec, MSE)           % Plot mean square quantization error
+plot(n_vec, MSE , 'r')           % Plot mean square quantization error
 title("quantization using compand function")
 xlabel('Number of bits')
 ylabel('Mean square quantization error')
