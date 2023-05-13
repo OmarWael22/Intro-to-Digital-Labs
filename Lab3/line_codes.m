7f2a1d6c6b975013403b90bfe1211fbba7aaf945
Omar Hany <108630571+es-OmarHani@users.noreply.github.com>
2023-05-14 00:23:44 +0300

    Update line_codes.m
    fix: update plots of PSD

--- Lab3/line_codes.m
+++ Lab3/line_codes.m
@@ -124,92 +124,23 @@ for k = 1:numel(pam_signals)
     xlabel('Time (s)');
 end
 
-% Plotting PSD
+% Calculate power spectral density
+[P_nrtz, f_nrtz] = periodogram(nrtz,[],[],1/Ts,'centered');
+[P_nrzi, f_nrzi] = periodogram(nrzi,[],[],1/Ts,'centered');
+[P_rtz, f_rtz] = periodogram(rtz,[],[],1/Ts,'centered');
+[P_ami, f_ami] = periodogram(ami,[],[],1/Ts,'centered');
+[P_manchester, f_manchester] = periodogram(MAN,[],[],1/Ts,'centered');
+[P_mlt, f_mlt] = periodogram(mlt,[],[],1/Ts,'centered');
+
+% Plot power spectral density
 figure;
-for k = 1:numel(pam_signals)
-    
-N = length(pam_signals{k}); % number of samples
-Fs = 1/Ts; % sampling frequency
-f = linspace(-Fs/2, Fs/2, N);% frequency vector
-
-subplot(numel(pam_signals), 1, k);
-plot(f, 10*log10(abs(fftshift(fft(pam_signals{k})) .^ 2)));
-xlim([-Fs/2, Fs/2]); grid on;
-title(sprintf('PSD for %s', upper(signals_names{k})));
+subplot(6,1,1); plot(f_nrtz,10*log10(P_nrtz)); title('Non-return to zero');
+subplot(6,1,2); plot(f_nrzi,10*log10(P_nrzi)); title('Non-return to zero inverted');
+subplot(6,1,3); plot(f_rtz,10*log10(P_rtz)); title('Return to zero');
+subplot(6,1,4); plot(f_ami,10*log10(P_ami)); title('Alternate mark inversion');
+subplot(6,1,5); plot(f_manchester,10*log10(P_manchester)); title('Manchester coding');
+subplot(6,1,6); plot(f_mlt,10*log10(P_mlt)); title('Multi-level transmission');
 xlabel('Frequency (Hz)');
-ylabel('Power/Frequency (dB/Hz)');
-end
+ylabel('Power spectral density (dB/Hz)');
 
-% 
-% % Apply pulse amplitude modulation (PAM) to NRZI signal
-% pam_signal_nrzi = zeros(1, length(nrzi*pulse_width));
-% for i = 1:length(nrzi)
-%     start_idx = (i-1)*pulse_width+1;
-%     end_idx = start_idx+pulse_width-1;
-%     pam_signal_nrzi(start_idx:end_idx) = nrzi(i) * pulse_shape;
-% end
-% 
-% % Apply pulse amplitude modulation (PAM) to NRZI signal
-% pam_signal_rtz = zeros(1, length(rtz*pulse_width));
-% for i = 1:length(rtz)
-%     start_idx = (i-1)*pulse_width+1;
-%     end_idx = start_idx+pulse_width-1;
-%     pam_signal_rtz(start_idx:end_idx) = rtz(i) * pulse_shape;
-% end
-% 
-% % Apply pulse amplitude modulation (PAM) to NRZI signal
-% pam_signal_ami = zeros(1, length(ami*pulse_width));
-% for i = 1:length(ami)
-%     start_idx = (i-1)*pulse_width+1;
-%     end_idx = start_idx+pulse_width-1;
-%     pam_signal_ami(start_idx:end_idx) = ami(i) * pulse_shape;
-% end
-% 
-% % Apply pulse amplitude modulation (PAM) to NRZI signal
-% pam_signal_man = zeros(1, length(MAN*pulse_width));
-% for i = 1:length(MAN)
-%     start_idx = (i-1)*pulse_width+1;
-%     end_idx = start_idx+pulse_width-1;
-%     pam_signal_man(start_idx:end_idx) = MAN(i) * pulse_shape;
-% end
-% 
-% % Apply pulse amplitude modulation (PAM) to NRZI signal
-% pam_signal_mlt = zeros(1, length(mlt*pulse_width));
-% for i = 1:length(mlt)
-%     start_idx = (i-1)*pulse_width+1;
-%     end_idx = start_idx+pulse_width-1;
-%     pam_signal_mlt(start_idx:end_idx) = mlt(i) * pulse_shape;
-% end
-
-
-% 
-% % Plot modulated signals
-% figure;
-% subplot(6,1,1); plot(pam_signal_nrtz); axis([0 length(pam_signal_nrtz) -1.5 1.5]); ylim([-1.5 1.5]); title('Non-return to zero');
-% subplot(6,1,2); plot(pam_signal_nrzi); axis([0 length(pam_signal_nrzi) -1.5 1.5]);ylim([-1.5 1.5]); title('Non-return to zero inverted');
-% subplot(6,1,3); plot(pam_signal_rtz); axis([0 length(pam_signal_rtz) -1.5 1.5]); ylim([-1.5 1.5]); title('Return to zero');
-% subplot(6,1,4); plot(pam_signal_ami); axis([0 length(pam_signal_ami) -1.5 1.5]); ylim([-1.5 1.5]); title('Alternate mark inversion');
-% subplot(6,1,5);plot(pam_signal_man); axis([0 length(pam_signal_man) -1.5 1.5]); ylim([-1.5 1.5]); title('Manchester coding');
-% subplot(6,1,6); plot(pam_signal_mlt); axis([0 length(pam_signal_mlt) -1.5 1.5]); ylim([-1.5 1.5]); title('Multi-level transmission');
-% xlabel('Time (s)');
-% 
-% % Calculate power spectral density
-% [P_nrtz, f_nrtz] = periodogram(nrtz,[],[],1/Ts,'centered');
-% [P_nrzi, f_nrzi] = periodogram(nrzi,[],[],1/Ts,'centered');
-% [P_rtz, f_rtz] = periodogram(rtz,[],[],1/Ts,'centered');
-% [P_ami, f_ami] = periodogram(ami,[],[],1/Ts,'centered');
-% [P_manchester, f_manchester] = periodogram(MAN,[],[],1/Ts,'centered');
-% [P_mlt, f_mlt] = periodogram(mlt,[],[],1/Ts,'centered');
-% 
-% % Plot power spectral density
-% figure;
-% subplot(6,1,1); plot(f_nrtz,10*log10(P_nrtz)); title('Non-return to zero');
-% subplot(6,1,2); plot(f_nrzi,10*log10(P_nrzi)); title('Non-return to zero inverted');
-% subplot(6,1,3); plot(f_rtz,10*log10(P_rtz)); title('Return to zero');
-% subplot(6,1,4); plot(f_ami,10*log10(P_ami)); title('Alternate mark inversion');
-% subplot(6,1,5); plot(f_manchester,10*log10(P_manchester)); title('Manchester coding');
-% subplot(6,1,6); plot(f_mlt,10*log10(P_mlt)); title('Multi-level transmission');
-% xlabel('Frequency (Hz)');
-% ylabel('Power spectral density (dB/Hz)');
-% 
 
